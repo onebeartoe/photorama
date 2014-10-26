c66b8ffb5ac7b069d44e2c9daf2e91432d22b586
onebeartoe <onebeartoe@gmail.com>
2014-10-26 08:59:52 -0500

    A different API was used to control the Adafruit LCD with keypad breakout.

--- photorama-raspberry-pi-lcd-keypad/src/main/java/org/onebeartoe/electronics/photorama/raspberry/pi/LcdKeypadPhotoramaApp.java
+++ photorama-raspberry-pi-lcd-keypad/src/main/java/org/onebeartoe/electronics/photorama/raspberry/pi/LcdKeypadPhotoramaApp.java
@@ -1,178 +1,154 @@
-
 package org.onebeartoe.electronics.photorama.raspberry.pi;
 
-import com.oracle.raspberry.pi.lcd.AdafruitLcdPlate;
-import com.oracle.raspberry.pi.lcd.Lcd;
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.PinPullResistance;
-import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
-import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
-import java.util.concurrent.Callable;
+//import com.oracle.raspberry.pi.lcd.AdafruitLcdPlate;
+//import com.oracle.raspberry.pi.lcd.Lcd;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.onebeartoe.electronics.photorama.Camera;
 import org.onebeartoe.electronics.photorama.RaspberryPiCamera;
+import se.hirt.pi.adafruitlcd.Button;
+import static se.hirt.pi.adafruitlcd.Button.DOWN;
+import static se.hirt.pi.adafruitlcd.Button.RIGHT;
+import static se.hirt.pi.adafruitlcd.Button.SELECT;
+import static se.hirt.pi.adafruitlcd.Button.UP;
+import se.hirt.pi.adafruitlcd.ButtonListener;
+import se.hirt.pi.adafruitlcd.ButtonPressedObserver;
+import se.hirt.pi.adafruitlcd.Color;
+import se.hirt.pi.adafruitlcd.ILCD;
+import se.hirt.pi.adafruitlcd.demo.AutoScrollDemo;
+import se.hirt.pi.adafruitlcd.demo.ColorDemo;
+import se.hirt.pi.adafruitlcd.demo.CursorDemo;
+import se.hirt.pi.adafruitlcd.demo.DisplayDemo;
+import se.hirt.pi.adafruitlcd.demo.ExitTest;
+import se.hirt.pi.adafruitlcd.demo.HelloWorldTest;
+import se.hirt.pi.adafruitlcd.demo.LCDTest;
+import se.hirt.pi.adafruitlcd.demo.ScrollTest;
+import se.hirt.pi.adafruitlcd.impl.RealLCD;
+import se.hirt.pi.adafruitlcd.impl.RealLCD.Direction;
 
 /**
  * @author Roberto Marquez
  */
 public class LcdKeypadPhotoramaApp
 {
+
     static final int BUS_NO = 1;  // 0 for Raspberry Pi B version 1 and 1 for version 2
     static final int BUS_ADDRESS = 0x20;
-    
+
     private Logger logger;
 
     private Camera camera;
 
-    private Lcd lcd;
-    
-    private GpioController gpio;
-    
-    public LcdKeypadPhotoramaApp() 
+    private ILCD lcd;
+
+    private static int currentTest = -1;
+
+    private final static LCDTest[] TESTS =
+    {
+        new HelloWorldTest(),
+        new ScrollTest(), new CursorDemo(), new DisplayDemo(),
+        new ColorDemo(), new AutoScrollDemo(), new ExitTest()
+    };
+
+    public LcdKeypadPhotoramaApp()
     {
         logger = Logger.getLogger(LcdKeypadPhotoramaApp.class.getName());
-        
-        try 
+
+        try
         {
             camera = new RaspberryPiCamera();
 
-            lcd = new AdafruitLcdPlate(BUS_NO, BUS_ADDRESS);
-            lcd.setBacklight(Lcd.BACKLIGHT_ON);
-            lcd.write("Photorama LCD!");
-            
-            Thread.sleep(1000);
+            lcd = new RealLCD();
+            lcd.setBacklight(Color.ON);
 
-            gpio = GpioFactory.getInstance();
+            lcd.setText("LCD Test!\nPress up/down...");
 
-            configureKeypadPins(gpio, camera, lcd);
-        } 
-        catch (Exception ex) 
+            ButtonPressedObserver observer = new ButtonPressedObserver(lcd);
+            observer.addButtonListener(new ButtonListener()
+            {
+                @Override
+                public void onButtonPressed(Button button)
+                {
+                    try
+                    {
+                        switch (button)
+                        {
+                            case UP:
+                                currentTest = --currentTest < 0 ? 0 : currentTest;
+                                lcd.clear();
+                                lcd.setText(String.format("#%d:%s\nPress Sel to run!",
+                                        currentTest, TESTS[currentTest].getName()));
+                                break;
+                            case DOWN:
+                                currentTest = ++currentTest > (TESTS.length - 1) ? TESTS.length - 1
+                                        : currentTest;
+                                lcd.clear();
+                                lcd.setText(String.format("#%d:%s\nPress Sel to run!",
+                                        currentTest, TESTS[currentTest].getName()));
+                                break;
+                            case RIGHT:
+                                lcd.scrollDisplay(Direction.LEFT);
+                                break;
+                            case LEFT:
+                                lcd.scrollDisplay(Direction.RIGHT);
+                                break;
+                            case SELECT:
+                                runTest(currentTest);
+                                break;
+                            default:
+                                lcd.clear();
+                                lcd.setText(String.format(
+                                        "Button %s\nis not in use...",
+                                        button.toString()));
+                        }
+                    }
+                    catch (IOException e)
+                    {
+                        handleException(e);
+                    }
+                }
+
+                private void runTest(int currentTest)
+                {
+                    LCDTest test = TESTS[currentTest];
+                    System.out.println("Running test " + test.getName());
+                    try
+                    {
+                        test.run(lcd);
+                    }
+                    catch (IOException e)
+                    {
+                        handleException(e);
+                    }
+                }
+            });
+
+            System.out.println("Press enter to quit!");
+            System.in.read();
+            lcd.stop();
+        }
+        catch (Exception ex)
         {
             logger.log(Level.SEVERE, null, ex);
         }
     }
 
-    public void configureKeypadPins(GpioController gpio, final Camera camera, final Lcd lcd)
+    private static void handleException(IOException e)
     {
-        
-        // button numbering SELECT = 0 
-        // RIGHT = 1 
-        // DOWN = 2 
-        // UP = 3 
-        // LEFT = 4
-        // provision gpio pin #00 as an input pin with its internal pull down resistor enabled
-        // (configure pin edge to both rising and falling to get notified for HIGH and LOW state
-        // changes)
-        GpioPinDigitalInput selectButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00,             // PIN NUMBER
-                                                                       "Select Button",                   // PIN FRIENDLY NAME (optional)
-                                                                       PinPullResistance.PULL_DOWN); // PIN RESISTANCE (optional)
-        selectButton.addListener(new GpioPinListenerDigital()
-        {
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent gpdsce) 
-            {
-                System.out.println("select");
-                lcd.clear();
-                lcd.write("Select");
-
-                camera.takeSnapshot();
-            }
-        });
-        selectButton.addTrigger( new GpioCallbackTrigger( new Callable<Void>() 
-        {
-            public Void call() throws Exception 
-            {
-                System.out.println("hey hey select");
-                lcd.clear();
-                lcd.write("Select");
-
-                camera.takeSnapshot();     
-
-                return null;
-            }
-        }));
-
-        GpioPinDigitalInput rightButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01,             // PIN NUMBER
-                                                                       "Right Button",                   // PIN FRIENDLY NAME (optional)
-                                                                       PinPullResistance.PULL_DOWN); // PIN RESISTANCE (optional)
-        rightButton.addListener(new GpioPinListenerDigital()
-        {
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent gpdsce) 
-            {
-                System.out.println("right");
-                lcd.clear();
-                lcd.write("Right");
-            }
-        });
-
-/*            
-        GpioPinDigitalInput downButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02,             // PIN NUMBER
-                                                                       "Down Button",                   // PIN FRIENDLY NAME (optional)
-                                                                       PinPullResistance.PULL_DOWN); // PIN RESISTANCE (optional)
-        downButton.addListener(new GpioPinListenerDigital()
-        {
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent gpdsce) 
-            {
-                System.out.println("down");
-
-                lcd.clear();
-                lcd.write("Down");
-            }
-        });
-
-        GpioPinDigitalInput upButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03,             // PIN NUMBER
-                                                                       "Up Button",                   // PIN FRIENDLY NAME (optional)
-                                                                       PinPullResistance.PULL_DOWN); // PIN RESISTANCE (optional)
-        upButton.addListener(new GpioPinListenerDigital() 
-        {
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent gpdsce) 
-            {
-                System.out.println("up");
-
-                lcd.clear();
-                lcd.write("Up");
-            }
-        });
-*/
-
-        GpioPinDigitalInput leftButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04,             // PIN NUMBER
-                                                                       "Left Button",                   // PIN FRIENDLY NAME (optional)
-                                                                       PinPullResistance.PULL_DOWN); // PIN RESISTANCE (optional)
-        leftButton.addListener(new GpioPinListenerDigital() 
-        {
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent gpdsce) 
-            {
-                System.out.println("left");
-
-                lcd.clear();
-                lcd.write("left");
-            }
-        });        
+        System.out.println("Problem talking to LCD! Exiting!");
+        e.printStackTrace();
+        System.exit(2);
     }
-    
+
     public static void main(String[] args) throws Exception
     {
         LcdKeypadPhotoramaApp app = new LcdKeypadPhotoramaApp();
-        
+
         // keep program running until user aborts (CTRL-C)
-        for (;;) 
+        for (;;)
         {
             Thread.sleep(500);
-        }        
-        
-//        System.out.println("end of run");
-    }
-
-    private void shutdown() 
-    {
-// call this on exit        
-        lcd.shutdown();
-
-        gpio.shutdown();
+        }
     }
 }
-
