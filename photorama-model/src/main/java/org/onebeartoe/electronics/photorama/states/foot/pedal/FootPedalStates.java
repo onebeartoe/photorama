82490ecd69caccc4f662acff20f86703b189d7f4
onebeartoe <onebeartoe@gmail.com>
2015-03-31 13:22:12 -0500

    The label was updated to snapshot instead of foot pedal.

--- photorama-model/src/main/java/org/onebeartoe/electronics/photorama/states/foot/pedal/FootPedalStates.java
+++ photorama-model/src/main/java/org/onebeartoe/electronics/photorama/states/foot/pedal/FootPedalStates.java
@@ -6,11 +6,12 @@ import org.onebeartoe.electronics.photorama.states.RootState;
 /**
  * @author Roberto Marquez
  */
+//TODO: rename this class to SnapshotStates
 public class FootPedalStates extends RootState
 {
     @Override
     public String getLabel()
     {
-        return "Foot Pedal";
+        return "Snapshot";
     }
 }
